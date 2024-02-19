48a0d02dbb63d95d76ffedd66dcb0a23007d99be
Christopher Desmond <104523163+christopherdsmd@users.noreply.github.com>
2024-02-19 17:05:44 -0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -71,7 +71,7 @@ export default function Login() {
     return (
         <div className="form-box">
             <form onSubmit={loginUser} type="login">
-                <h2>Welcome to Habit! <br /><br />
+                <h2>Welcome to Habbit! <br /><br />
                     <img src="\assets\frog_login.png" width="128" height="128" alt="Frog_login" /> <br />Login</h2>
                 <label></label>
                 <input type="email" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} placeholder="Email" /><br />
@@ -81,7 +81,7 @@ export default function Login() {
     
                 <button type="submit">Login</button>
             </form>
-            <br /> <br /> <br />
+            <br /> <br /> <br /><br />
             <hr className="solidline" />
             <h3>Don't have an Account? </h3>
             <button className="Register-button" onClick={handleRegisterClick}>Register</button> 
