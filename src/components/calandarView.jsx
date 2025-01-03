ffd7943534fc690bdbb093f3ba300b6592249738
Christopher Desmond <104523163+christopherdsmd@users.noreply.github.com>
2025-01-02 22:04:34 -0500

    bugfix calandar view with year
    bugfix

--- src/components/calandarView.jsx
+++ src/components/calandarView.jsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React from 'react';
 import './calandarView.css';
 import CalendarHeatmap from 'react-calendar-heatmap';
 import 'react-calendar-heatmap/dist/styles.css';
-import 'react-tooltip/dist/react-tooltip.css';
-import { Tooltip } from 'react-tooltip';
-import axios from 'axios';
-import { UserContext } from '../context/userContext';
+import 'react-tooltip/dist/react-tooltip.css'
+import {Tooltip} from 'react-tooltip';
 
 const generateDateValues = (habitDates, startDate, endDate) => {
   const dateValues = [];
@@ -13,14 +11,25 @@ const generateDateValues = (habitDates, startDate, endDate) => {
 
   while (currentDate <= endDate) {
     const dateString = currentDate.toISOString().split('T')[0];
-    const count =
-      habitDates?.find((habitDate) => habitDate.date === dateString)?.count || 0;
+    const count = habitDates?.find((habitDate) => habitDate.date === dateString)?.count || 0;
     dateValues.push({ date: dateString, count });
     currentDate.setDate(currentDate.getDate() + 1);
   }
 
   return dateValues;
 };
+
+// Get first date of current year
+function getFirstDayOfCurrentYear() {
+  const currentYear = new Date().getFullYear();
+  return `${currentYear}-01-01`;
+}
+
+// Get last date of current year
+function getLastDayOfCurrentYear() {
+  const currentYear = new Date().getFullYear();
+  return `${currentYear}-12-31`;
+}
 
 const formatDate = (dateString) => {
   const date = new Date(dateString);
@@ -29,9 +38,8 @@ const formatDate = (dateString) => {
 };
 
 const CalendarView = ({ habits }) => {
-  const startDate = new Date('2024-01-01');
-  const endDate = new Date('2024-12-31');
-  const { user } = useContext(UserContext); // Get user data from context
+  const startDate = new Date(getFirstDayOfCurrentYear());
+  const endDate = new Date(getLastDayOfCurrentYear());
 
   return (
     <div>
@@ -46,24 +54,24 @@ const CalendarView = ({ habits }) => {
             startDate={startDate}
             endDate={endDate}
             values={generateDateValues(habit.daily_check, startDate, endDate)}
-            classForValue={(value) => {
+            classForValue={value => {
               if (!value || value.count === 0) {
                 return 'color-empty';
               }
               return `color-github-${value.count}`;
             }}
             showOutOfRangeDays={true}
-            tooltipDataAttrs={(value) => ({
-              'data-tip': `${value.date} has count: ${value.count}`,
-            })}
+            tooltipDataAttrs={value => {
+              return {
+                'data-tip': `${value.date} has count: ${
+                  value.count
+                }`,
+              };
+            }}
             showWeekdayLabels={true}
-            onClick={(value) =>
-              alert(
-                `On ${formatDate(value.date)} You Completed '${habit.habit_name}' ${value.count} times.`
-              )
-            }
-          />
-          <Tooltip id="calendar-tooltip" />
+            onClick={value => alert(`On ${formatDate(value.date)} You Completed '${habit.habit_name}' ${value.count} times.`)}
+            />
+          <Tooltip id="calendar-tooltip" /> 
         </div>
       ))}
     </div>
